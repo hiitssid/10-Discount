faf7b741351726d66d5096947f0046ce2684b2bc
hiitssid <see.siddhartha@gmail.com>
2023-04-09 10:11:29 +0545

    added localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,49 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import Card from "./Card";
+import "./App.css";
+const localStorageKey = 'thisIsKey'
 export default function App() {
+  //initialze state, mrp variable is set to store the number from input element
   const [mrp, setMRP] = useState([]);
+  //sellingPrice is calculated at 10% discount
   const sellingPrice = mrp - mrp * 0.1;
   console.log(`MRP:${mrp}, SP:${sellingPrice}`);
+  //every keystroke is saved to state mrp with function setMRP, dom method of (event.target.value) is used to get the value from input field
 
   function handleChange(event) {
     setMRP(event.target.value);
   }
 
-  const [price, setPrice] = useState([{mrp:'2',sp:'2'}])
-  console.log(price)
-  function handleClick(){
-    setPrice(prevState=>[...prevState, {mrp,sellingPrice}])
+  //array price is decleared with empty object to store the mrp and sellingPrice data in the object
+  //then later while developing and research in Stackoverflow, it is suggested that JSON.parse to read from localStorage directly when setting the initial
+  //https://stackoverflow.com/questions/72222728/why-is-localstorage-getting-cleared-whenever-i-refresh-the-page
+  const [price, setPrice] = useState(() => {
+    return JSON.parse(localStorage.getItem(localStorageKey)) || [];
+  });
+
+  //when pressed 'Add' button, new object is added to price array with mrp and sellingPrice,
+  function handleClick() {
+    //...prevState will spread the older elements in the array and {mrp:mrp,sp:sellingPrice} will add current object to the array
+    setPrice((prevState) => [...prevState, { mrp: mrp, sp: sellingPrice }]);
   }
+  //setItem to localStorege : 
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(price));
+  }, [price]);
+
+const sortedPrice = price.sort((a,b)=>a.mrp - b.mrp);
+const card = sortedPrice.map(item => {
+  return <Card key={item.index} mrp={item.mrp} sp={item.sp}/>
+})
 
   return (
-    <>
-      <div className="App">
+    <div className="app-wrapper">
+      <div className="top-form-card">
+        <h1>{sellingPrice}</h1>
         <input type="text" onChange={handleChange} />
         <button onClick={handleClick}>Add</button>
       </div>
-      <div>{/* render here */}</div>
-    </>
+        {card}
+    </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,9 @@
+import './Card.css'
 export default function Card(props) {
     return (
       <div class="sellCard">
-        <p>{props.mrp}</p>
-        <p>{props.sp}</p>
+        <h1 className='sellPrice'>{props.sp}</h1>
+        <h4>{props.mrp}</h4>
       </div>
     );
   }
